6294449fcb594194c4737e48879600de53ea5669
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-12-12 23:34:06 +0900

    Solved errors by implicit any.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+// https://nextjs.org/docs/basic-features/typescript#custom-app
+import type { AppProps /*, AppContext */ } from 'next/app';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+// https://nextjs.org/docs/basic-features/typescript#api-routes
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  res.statusCode = 200;
+  res.json({ name: 'John Doe' });
+};
