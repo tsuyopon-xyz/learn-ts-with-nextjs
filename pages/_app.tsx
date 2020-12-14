5db66ab35913479c75af71c2e798b8e2e61b80f3
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-12-14 20:33:17 +0900

    Setup MmaterialUI env.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,41 @@
 // https://nextjs.org/docs/basic-features/typescript#custom-app
+import { useEffect } from 'react';
+import Head from 'next/head';
 import type { AppProps /*, AppContext */ } from 'next/app';
-import '../styles/globals.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Theme from 'src/components/Theme';
+import AppBar from 'src/components/layouts/AppBar';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      // JavaScript: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+      // TypeScript: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-7.html
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ThemeProvider theme={Theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="ja">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
